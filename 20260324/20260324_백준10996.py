deb38d123aebc2349ab3609eb59bb278a36db392
Rayarde <purpllesg0707@gmail.com>
2026-03-29 23:57:21 +0900

    과제

--- 20260324/20260324_백준10996.py
+++ 20260324/20260324_백준10996.py
@@ -1,8 +1,10 @@
 star = int(input())
 
-for i in range(1, star+1):
-    if (star%2 == 1):
-        a = star//2
-        for j in range(a):
-            print("* ")
-    print(f"{"*"*i}{"\b"*star}")
+if (star%2 == 0):
+    for i in range(1, star+1):
+        print("* "*(star//2))
+        print(" *"*(star//2))
+else:
+    for i in range(1, star+1):
+        print("*"+" *"*(star//2))
+        print(" *"*(star//2))
