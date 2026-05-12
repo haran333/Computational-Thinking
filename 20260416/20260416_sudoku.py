b65674e7c780080f7ed32b827befb76ca57ecf2a
Rayarde <purpllesg0707@gmail.com>
2026-05-12 11:40:31 +0900

    과제

--- 20260416/20260416_sudoku.py
+++ 20260416/20260416_sudoku.py
@@ -1,52 +1,46 @@
-tfa = [0,1,2],[3,4,5], [6,7,8]
-puzzle = []
-for i in range(9):
-    puzzle.append(list(map(int,input().split())))
-def board_33(board, row, col):
-    board33 = []
-    g, s = 0, 0
-    for i, sublist in eNerate(tfa):
-        if row in sublist:
-            g = i
-            break
-    for i, sublist in eNerate(tfa):
-        if col in sublist:
-            s = i
-            break
-    for i in tfa[g]:
-        for j in tfa[s]:
-            board33.append(board[i][j])
-    return board33
+puzzle = [
+        [4,2,9,0,0,1,0,5,8],
+        [0,0,8,4,0,9,0,0,2],
+        [0,7,0,8,0,3,0,9,0],
+        [0,8,0,9,0,0,0,0,3],
+        [0,0,3,0,0,0,5,0,0],
+        [5,0,0,0,0,2,0,8,0],
+        [0,5,0,2,0,7,0,3,0],
+        [3,0,0,5,0,4,8,0,0],
+        [9,6,0,1,0,0,4,7,5]
+    ]
 
-def is_valid(board,row,col,N):
-    if N < 1 or N > 9:
+def is_valid(board, row, col, num):
+    # col check
+    if num in set(board[col]):
         return False
-    board33 = board_33(board, row, col)
-    garo = board[row]
-    sero = []
-    for i in range(0, 9):
-        sero.append(board[i][col])
-    if N not in garo:
-        if N not in sero:
-            if N not in board33:
-                return True
-            else:
-                return False
-        else:
+
+    # row check
+    for i in range(9):
+        if num == board[i][row]:
             return False
-    else:
+
+    # 3*3 box check
+    boxList = []
+    for i in board[3*(col//3): 3*(col//3)+3]:
+        boxList += i[3*(row//3): 3*(row//3)+3]
+    if num in set(boxList):
         return False
+
+    # num is available
+    return True
 
 def solve_sudoku(board):
-    for i in range(0, 9):
-        for j in range(0, 9):
-            if board[i][j] == 0:
-                for N in range(1, 10):
-                    if is_valid(board, i, j, N):
-                        board[i][j] = N
+    for y in range(9):
+        for x in range(9):
+            if board[y][x] == 0:                # target is empty
+                for N in range(1, 10):          # input 1~9
+                    if is_valid(board, x, y, N):
+                        board[y][x] = N
                         if solve_sudoku(board):
                             return True
-                    board[i][j] = 0
+                        else:
+                            board[y][x] = 0
                 return False
     return True
 
